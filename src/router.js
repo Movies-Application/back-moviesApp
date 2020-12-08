411a22883257b78ed76bf6451c5043e2f5ccfd0a
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-08 19:47:22 +0200

    register POST added (without auth)

---
+++ src/router.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql");
+const con = require("./database");
+
+router.get("/", (req, res) => {
+  res.send("This boilerplate is working!");
+});
+
+router.post("/register", (req, res) => {
+  const username = req.body.username.toLowerCase();
+  const password = req.body.password;
+
+  con.query(
+    `SELECT * FROM users WHERE username = ${mysql.escape(username)}`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ msg: "Internal server error checking username validity" });
+      } else if (result.length !== 0) {
+        return res.status(400).json({ msg: "Username already exists." });
+      } else {
+        con.query(
+          `INSERT INTO users (username, password) VALUES (${mysql.escape(
+            username
+          )}, ${mysql.escape(password)})`,
+          (er, result) => {
+            if (err) {
+              console.log(err);
+              return res
+                .status(400)
+                .json({ msg: "Internal server error saving user details." });
+            } else {
+              return res
+                .status(201)
+                .json({ msg: "New user has been successfully registered!" });
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+module.exports = router;
