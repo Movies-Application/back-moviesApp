894647087b0ce4fc2fdcabfed83ed6bd8c66b4ec
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-08 19:22:24 +0200

    database connection exported to other .js file

--- server.js
+++ server.js
@@ -6,19 +6,6 @@ require("dotenv").config();
 
 const port = process.env.SERVER_PORT || 3000;
 
-const con = mysql.createConnection({
-  host: process.env.MYSQL_DB_HOST,
-  user: process.env.MYSQL_DB_USER,
-  password: process.env.MYSQL_DB_PASS,
-  database: process.env.MYSQL_DB_NAME,
-  port: process.env.MYSQL_DB_PORT,
-});
-
-con.connect((err) => {
-  if (err) throw err;
-  console.log("Successfully connected to DB");
-});
-
 const app = express();
 
 app.use(bodyParser.json());
