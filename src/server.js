19f2a9408b02734c720a877caf8a5e4fee2e8dc6
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-08 19:46:37 +0200

    router.js imported

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
+const routes = require("./router");
 
 const port = process.env.SERVER_PORT || 3000;
 
@@ -10,8 +11,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("This boilerplate is working!");
-});
+app.use("/", routes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
