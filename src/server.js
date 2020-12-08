1970cc04f0b3c11d09f080893f42c051419d40c4
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-08 19:24:32 +0200

    server.js file path updates

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.SERVER_PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("This boilerplate is working!");
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
